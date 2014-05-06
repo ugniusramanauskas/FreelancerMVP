61c6c76832c30eded289a13b7b54c55eb5062c5b
Justin Cheng <justincheng94@gmail.com>
2014-05-06 03:03:06 +0100

    revert

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new TGC\AdminBundle\TGCAdminBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

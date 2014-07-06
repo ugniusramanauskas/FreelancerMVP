cbbe270edc79e6ddceb981692c8b436c4ae118a7
Argyrios Gounaris <agounaris@gmail.com>
2014-07-06 18:49:09 +0100

    ADDED: new design, a lot of visual things to fix

---
+++ src/TGC/AdminBundle/Controller/HomeController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: argi
+ * Date: 06/07/2014
+ * Time: 12:31
+ */
+
+namespace TGC\AdminBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends Controller {
+
+    public function indexAction()
+    {
+        return $this->render('TGCAdminBundle:Home:index.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        if ($request->getMethod() === 'POST') {
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Web site contact from '.$request->request->get('name'))
+                ->setFrom('agounaris@gmail.com')
+                ->setTo('agounaris@gmail.com')
+                ->setBody($request->request->get('email').' says: '.$request->request->get('message'))
+            ;
+            $this->get('mailer')->send($message);
+
+            return $this->render('TGCAdminBundle:Home:thankyou.html.twig');
+        }
+
+        return $this->render('TGCAdminBundle:Home:contact.html.twig');
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('TGCAdminBundle:Home:about.html.twig');
+    }
+
+} 
